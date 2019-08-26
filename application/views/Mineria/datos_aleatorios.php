711979ba348577b9ac27b3377a7356c4e16b0cb9
Alexander Rangel <alexander.rangel@alumno.buap.mx>
2019-08-26 12:11:37 -0500

    Tabla dos invent que tiene 100000 registros con dos campos adicionales.

--- application/views/Mineria/datos_aleatorios.php
+++ application/views/Mineria/datos_aleatorios.php
@@ -11,7 +11,7 @@ if ($db == false)
 $abecedario = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 $mesBisiesto = [30, 29, 31, 30, 31, 30, 30, 31, 30, 29, 30, 31];
 $mesNomal = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-for ($i = 0; $i < 10000; $i++)
+for ($i = 0; $i < 100000; $i++)
 {
   $producto = $abecedario[rand(0, 25)] . (string)rand(0, 12);
   $almacen = (string)rand(1, 20);
@@ -29,7 +29,8 @@ for ($i = 0; $i < 10000; $i++)
   $fecha = (string)$ano . "-" . (string)$mes . "-" . (string)$dia;
   $importe = rand(1, 1000);
   $provedor = $abecedario[rand(0, 25)] . $abecedario[rand(0, 25)];
-  $sql = "INSERT INTO invent(producto, almacen, cantidad, fecha, importe, provedor) values ('$producto', '$almacen', '$cantidad', '$fecha', '$importe', '$provedor')";
+  $codigo = $abecedario[rand(0, 25)] . $abecedario[rand(0, 25)] . (string)rand(0, 9) . (string)rand(0, 9) . $abecedario[rand(0, 25)] . $abecedario[rand(0, 25)] . (string)rand(0, 9) . (string)rand(0, 9);
+  $sql = "INSERT INTO invent(producto, almacen, cantidad, fecha, importe, provedor, codigo) values ('$producto', '$almacen', '$cantidad', '$fecha', '$importe', '$provedor', '$codigo')";
   if(!$result = mysqli_query($db, $sql))
   {
       echo "eRROR";
